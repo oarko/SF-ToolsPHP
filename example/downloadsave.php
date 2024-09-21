59b3ee10acbf25c8263c4cb4cd5859c77e998137
ArrayBytes <49374653+oarko@users.noreply.github.com>
2024-09-21 14:22:09 -0400

    Update downloadsave.php

--- example/downloadsave.php
+++ example/downloadsave.php
@@ -1,10 +1,16 @@
 <?php
-if($_POST){
-    require 'config.php';
-    include 'SF_Tools.php';
-    $sf_tools = new SF_Tools($server_address, $server_port, true);
-    $sf_tools->setAPIkey("Bearer ". $api_key);
-    $sf_tools->downloadSave($_POST['savename']);    
+require 'config.php';
+include 'SF_Tools.php';
+$sf_tools = new SF_Tools($server_address, $server_port, true);
+$sf_tools->setAPIkey($api_key);
+
+if($_POST['submit'] == "download"){
+    $sf_tools->downloadSave($_POST['savename']);
+}elseif($_POST['submit'] == "delete"){
+    $sf_tools->deleteSave($_POST['savename']);
+    if($sf_tools->responce_code == 204){
+        header("Location: " . $_SERVER['HTTP_REFERER']);
+    }
 }else{
     echo "No POST data";
 }
